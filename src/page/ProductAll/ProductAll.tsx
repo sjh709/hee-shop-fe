46235bb2697da8a25d163539cb2d3070ef4783f9
Jihee <sjh709@naver.com>
2024-06-24 04:17:58 +0900

    랜딩페이지 수정

--- src/page/ProductAll/ProductAll.tsx
+++ src/page/ProductAll/ProductAll.tsx
@@ -5,35 +5,40 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import Paginate from '../../components/Paginate/Paginate';
 import { SearchQueryType } from '../../model/product';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { productActions } from '../../redux/actions/productAction';
 
 function ProductAll() {
   const [query, setQuery] = useSearchParams();
   const [searchQuery, setSearchQuery] = useState<SearchQueryType>({
-    page: query.get('page') || '1',
-    name: query.get('name') || '',
+    page: '1',
+    name: '',
   });
   const { productList, totalPageNum } = useSelector(
     (state: RootState) => state.product
   );
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handlePageClick = ({ selected }: { selected: number }) => {
     setSearchQuery({ ...searchQuery, page: String(selected + 1) });
   };
 
   useEffect(() => {
-    console.log('searchQuery');
-    dispatch(productActions.getProductList({ ...searchQuery, pageSize: 12 }));
-  }, [searchQuery]);
-
-  useEffect(() => {
     const page = query.get('page') || '1';
     const name = query.get('name') || '';
-    setSearchQuery({ ...setQuery, page, name });
-    dispatch(productActions.getProductList({ ...searchQuery, pageSize: 12 }));
+    setSearchQuery({ page, name });
+    dispatch(productActions.getProductList({ page, name, pageSize: 4 }));
   }, [query]);
+
+  useEffect(() => {
+    if (searchQuery.name === '') {
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    navigate('?' + query);
+  }, [searchQuery]);
 
   return (
     <Container className='mt-5 mb-5'>
